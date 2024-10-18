d92502be7ac1b142b845b97e6813e7ab29e7b424
joaogson <126979581+joaogson@users.noreply.github.com>
2024-10-17 23:08:06 -0300

    Master

--- ValidadorCPF/ValidadorCPF/obj/Debug/net8.0/ValidadorCPF.AssemblyInfo.cs
+++ ValidadorCPF/ValidadorCPF/obj/Debug/net8.0/ValidadorCPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ValidadorCPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6496509d8310529e6cc648fbaba03ad2601618a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ValidadorCPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ValidadorCPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
